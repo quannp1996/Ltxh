0b26e8cd34682ef58e547e442ca93b3f25f3266c
quannp1996 <npquan1995@gmail.com>
2019-01-19 10:39:22 +0700

    Add files via upload

---
+++ MHNN/MHNN/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MHNN
+{
+    public partial class Form1 : Form
+    {
+        MHNN mohinh = new MHNN();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if(textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Yêu cầu nhập đầy đủ dữ liệu");
+                return;
+            }
+            mohinh.a        = Convert.ToInt16(textBox1.Text);
+            mohinh.b        = Convert.ToInt16(textBox2.Text);
+            mohinh.c        = Convert.ToInt16(textBox3.Text);
+            mohinh.d        = Convert.ToInt16(textBox4.Text);
+            string[] matran = new string[mohinh.d + 1];
+            matran = mohinh.matran();
+            listBox1.Items.Add("Ma trận sinh G");
+            for (int i = 0; i < matran.Length; i++)
+            {
+                listBox1.Items.Add(matran[i]);
+            }
+            listBox1.Items.Add(""); listBox1.Items.Add(""); 
+            float[] pio = mohinh.buildPi();
+            float soxe = 0;
+
+            for(int i=0; i< pio.Length; i++)
+            {
+                soxe += i * pio[i];
+                listBox1.Items.Add("pi("+i+") = "+pio[i]);
+            }
+            float ln = 0;
+            float r = mohinh.a / mohinh.b;
+            float Lq = (soxe - r);
+            float Wq = Lq / mohinh.a;
+            float W = Wq + 1 / mohinh.b;
+            listBox1.Items.Add("");
+            if (textBox5.Text != "") {
+                ln = soxe * 24 * (Convert.ToInt16(textBox5.Text));
+                listBox1.Items.Add("Lợi nhuận = " + ln);
+            }
+            listBox1.Items.Add("Số khách trung bình toàn bộ hệ thống là " + soxe);
+            //listBox1.Items.Add("Số khách trung bình trong hàng chờ là " + Lq.ToString());
+            //listBox1.Items.Add("Thời gian chờ trung bình trong hàng chờ "  + Wq.ToString());
+            //listBox1.Items.Add("Thời gian chờ trung bình trong toàn bộ hệ thống "  + W.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MHNN/MHNN/MHNN.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace MHNN
+{
+    class MHNN
+    {
+        public int a;
+        public int b;
+        public int c;
+        public int d;
+        public string[] matran()
+        {
+            string[] g = new string[this.d + 1];
+            for(int i = 0; i <= this.d; i++)
+            {
+                string hang = "";
+                int mul = 0;
+                if(i < this.c) { mul = i * this.b; } else { mul = this.c * this.b; }
+                for(int j = 0; j <= this.d; j++)
+                {
+                    if (i == 0)
+                    {
+                        if(j == i) { hang += "-" + this.a+ "  "; }
+                        else if(j == i + 1) { hang +=this.a+ "  "; }
+                        else { hang += "0  "; }
+                    }
+                    else if(i == this.d)
+                    {
+                        if (j == i - 1) { hang += mul + "  "; }
+                        else if (j == i) { hang += "-" + mul +"  "; }
+                        else { hang += "0  "; }
+                    }
+                    else
+                    {
+                        if(j == i - 1) { hang += mul+ "  "; }
+                        else if (j == i) { hang += "-" + (int) (this.a + mul)+"  "; }
+                        else if (j == i + 1) { hang += this.a+ "  "; }
+                        else { hang += "0  "; }
+                    }
+                }
+                g[i] = hang;
+            }
+            return g;
+        }
+        public float[] buildPi()
+        {
+            
+            float[] pii  = new float[this.d + 1];
+            float[] mul  = new float[this.d + 2];
+            float tong = 0;
+            //Tinh mang MUL
+            for (int i = 1; i <= this.d + 1; i++)
+            {
+                if (i < this.c) { mul[i] = i * this.b; } else { mul[i] = this.c * this.b; }
+            }
+            // Tính pi[0]
+            for (int i = 1; i <= this.d; i++)
+            {
+                string str = "";
+                float phanso = 1;
+                for (int j = 0; j< i; j++)
+                {
+                    phanso *= this.a / mul[j+1];
+                }
+                tong += phanso;
+            }
+            pii[0] = 1 / (1 + tong);
+            // Xác định các pi còn lại
+            for (int i = 1; i <= this.d; i++)
+            {
+                pii[i] = (this.a / mul[i]) * pii[i - 1];
+            }
+            return pii;
+        }
+    }
+}
